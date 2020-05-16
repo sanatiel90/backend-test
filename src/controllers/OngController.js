b0e49d5c51dd6709c55de535601f48a7ebaa5dc2
wilton procopio <wprocopio@gmail.com>
2020-05-16 15:54:40 -0300

    subindo primeiro commit

---
+++ src/controllers/OngController.js
@@ -0,0 +1,27 @@
+// conecção com banco pegando arquivo conection
+const conection = require('../database/conecction');
+const crypto = require ('crypto');
+
+
+module.exports = {
+    
+    async index (req,res)  {
+        // crio uma variavel ongs para fazere o select de tudo
+        const ongs = await conection('ongs').select('*');
+        return res.json(ongs);
+      },
+    
+    async create(req, res) {
+        // desetruturacao do java script para madnar apenas os campos corretos  
+        const { name, email, whatsapp, city, uf } = req.body;
+        // gerando o ID randomico
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await conection('ongs').insert({
+            id, name, email, whatsapp, city, uf
+        });
+
+        return res.json({ id });
+    }
+
+}
